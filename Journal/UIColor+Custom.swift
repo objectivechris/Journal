5d38f1aa2b322a12f0ded6746de41121e9196ee7
Rene <crene@msc-c02nr0amg3qplt.na.autotrader.int>
2017-05-22 16:16:04 -0400

    N?A

--- Journal/UIColor+Custom.swift
+++ Journal/UIColor+Custom.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2017 Christopher Rene. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        self.init(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1)
+    }
+}
